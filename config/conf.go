46d6f0c112e117e4c13232f11d7f0406018137a9
Emin.lin <emin.lin@zkteco.com>
2022-02-10 18:32:17 +0800

    init project

---
+++ config/conf.go
@@ -0,0 +1,12 @@
+package config
+
+var (
+	//新增短链限制 每分钟 /min
+	IssueRateLimit = 30
+	//MurmurHash Bit
+	MurmurBit = 32
+	//磁盘空间限制 M
+	DiskLimit = 512
+	//内存空间限制 M
+	MemoryLimit = 1024
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module GoShortURL
+
+go 1.17
