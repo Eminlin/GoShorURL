d255ab42e13a94ae0b961a57dc4caad1db23cb12
Eminlin <emin.lin@live.com>
2022-03-08 18:32:05 +0800

    新增错误结构体

--- model/msg.go
+++ model/msg.go
@@ -1,4 +1,17 @@
 package model
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type ErrMsg struct {
+	Code int    `json:"code"`
+	Msg  string `json:"errMsg"`
+}
+
+func CommonErrResp(w http.ResponseWriter, msg string) {
+	w.WriteHeader(406)
+	m, _ := json.Marshal(ErrMsg{Code: 406, Msg: msg})
+	w.Write(m)
 }
--- server/web.go
+++ server/web.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"GoShortURL/common"
+	"GoShortURL/model"
 	"net/http"
 	"unicode"
 )
@@ -11,12 +12,18 @@ var log common.Log
 //index short url request redict
 func index(w http.ResponseWriter, r *http.Request) {
 	if r.RequestURI == "/" || r.RequestURI == "/favicon.ico" {
+		model.CommonErrResp(w, "path is empty")
+		return
+	}
+	if r.Method != "GET" {
+		model.CommonErrResp(w, "invalid request method")
 		return
 	}
 	path := r.URL.Path
 	path = path[1:]
 	for _, v := range path {
 		if !unicode.IsLetter(v) && !unicode.IsDigit(v) || unicode.Is(unicode.Han, v) {
+			model.CommonErrResp(w, "invalid path")
 			return
 		}
 	}
